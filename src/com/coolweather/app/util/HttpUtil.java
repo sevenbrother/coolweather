ea2c18dfbb12f15b69f499294fcbfbafcce56f08
seven <seven_brother@163.com>
2016-08-03 16:11:37 +0800

    完成遍历省市县功能

---
+++ src/com/coolweather/app/util/HttpUtil.java
@@ -0,0 +1,53 @@
+package com.coolweather.app.util;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpUtil {
+	public static void sendHttpRequest(final String address, final HttpCallbackListener listener){
+		//开启子线程
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				HttpURLConnection connection = null;
+				try {
+					URL url = new URL(address);
+					
+					connection = (HttpURLConnection) url.openConnection();
+					connection.setRequestMethod("GET");
+					connection.setConnectTimeout(8000);
+					connection.setReadTimeout(8000);
+					
+					InputStream in = connection.getInputStream();
+					BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+					StringBuilder responseBuilder = new StringBuilder();
+					
+					String line;
+					while ((line = reader.readLine()) != null) {
+						responseBuilder.append(line);
+					}
+					
+					if (listener != null) {
+						//回调onFinish()方法
+						listener.onFinish(responseBuilder.toString());
+					}
+				} catch (Exception e) {
+					// TODO: handle exception
+					if (listener != null) {
+						//回调onError()方法
+						listener.onError(e);
+					}
+				} finally {
+					if (connection != null) {
+						connection.disconnect();
+					}
+				}
+			}
+		}).start();
+	}
+}
